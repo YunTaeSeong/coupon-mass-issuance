dc5f8925938bc4b2e10ea5043e760690c055de1f
YunTaeSeong <xoqkf1234@naver.com>
2026-02-18 14:39:37 +0900

    coupon: - 쿠폰 엔티티 및 스키마 작성

--- coupon-api/src/main/java/com/example/couponapi/controller/HelloController.java
+++ coupon-api/src/main/java/com/example/couponapi/controller/HelloController.java
@@ -7,7 +7,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @GetMapping("/hello")
-    public String hello() {
+    public String hello() throws InterruptedException {
+        // 초당 2건 처리 * N(서버에서 동시에 처리 가능한 수 = 톰캣에서 쓰레드 풀을 통해 요청을 처리(max = 200))
+        // => 400
+        Thread.sleep(500);
         return "hello!";
     }
 }
